502b02d63f1cbb4083e76c6bb2fafa2476b5abb6
Sebastian Termen <Sebastian.Termen@gmail.com>
2023-02-14 20:38:39 +0200

    Initial introduction to multithreading in c#.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// https://www.tutorialspoint.com/csharp/csharp_multithreading.htm
+using System;
+using System.Threading;
+
+namespace MultithreadedProgramming;
+class Program
+{
+    public static void ThreadStarter()
+    {
+        try
+        {
+            Console.WriteLine("Thread Activated!");
+            Console.WriteLine("Thread Will Sleep!");
+            Thread.Sleep(1000);
+            Console.WriteLine("Thread Will Awake!");
+
+            Console.WriteLine("Begin countdown!");
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+        catch (ThreadAbortException e)
+        {
+            Console.WriteLine("Thread Abort Exception");
+        }
+        finally
+        {
+            Console.WriteLine("Couldn't catch the Thread Exception");
+        }
+
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello, World!");
+        ThreadStart th = new ThreadStart(ThreadStarter);
+        Console.WriteLine("Seperating threads!");
+
+        Console.WriteLine("Activating Thread Process");
+        Thread childTh = new Thread(th);
+        childTh.Start();
+        Console.WriteLine("End Of Thread Activation Process");
+
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Abortion will happen!");
+        childTh.Interrupt();
+
+        Console.ReadKey();
+    }
+}
